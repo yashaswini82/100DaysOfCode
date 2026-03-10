1503227eb0b56e0bd013dea02c5df7638037a123
yashaswini82 <yashaswini2082@gmail.com>
2026-03-10 20:20:42 +0530

    Day 1: Band Name Generator project
    Completed Day 1 exercise from the 100 Days of Python course.
    This program generates a band name using the city name and pet name entered by the user.
    And i have learnt about print, input, variables and string manipulation.

---
+++ Day_01.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator")
+city_name=input("What is the name of the city you grew up in?\n")
+pet_name=input("What is the name of your pet?\n")
+print("Your band name is " + city_name + " " + pet_name)
+
